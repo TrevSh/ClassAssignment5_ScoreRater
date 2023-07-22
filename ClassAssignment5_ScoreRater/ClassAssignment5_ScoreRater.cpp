e70aa60c149fe79d9392aaf8fc5b97b161eef591
TrevSh <trevonshelby@outlook.com>
2023-07-22 14:48:01 -0500

    Added User Input and comments

--- ClassAssignment5_ScoreRater/ClassAssignment5_ScoreRater.cpp
+++ ClassAssignment5_ScoreRater/ClassAssignment5_ScoreRater.cpp
@@ -3,31 +3,27 @@
 
 int main()
 {
-    if (true) {
-        std::cout << "This is always displayed.\n\n";
-    }
-    if (false) {
-        std::cout << "This is never displayed\n";
-    }
-    int score = 1000;
+    int score;
+    std::cout << "Enter a score: ";
+    std::cin >> score;
 
-    if (score) {
-        std::cout << "Atleast you didnt get zero...\n";
+    if (score > 9000) {                                                 //The program will check each statement starting and execute the one with the proper conditions met.
+        std::cout << "Your score... ITS OVER 9000!!!\n";
     }
-    if (score >= 250) {
-        std::cout << "You scored 250 or more. Nice\n";
-    }
-    if (score >= 500) {
-        std::cout << "You scored 500 or more. Dope\n";
-    }
-    if (score >= 1000) {
-        std::cout << "You scored 1000 or more! Crazy!\n";
-    }
-    if (score >= 2000) {
+    else if (score >= 2000) {
         std::cout << "Your score is over 2000! WOW!\n";
     }
-    if (score > 9000) {
-        std::cout << "Your score... ITS OVER 9000!!!\n";
+    else if (score >= 1000) {
+        std::cout << "You scored 1000 or more! Crazy!\n";
+    }
+    else if (score >= 500) {                                            //else will execute a block of code only if the previous if is false. Can followed by another if (Like shown).
+        std::cout << "You scored 500 or more. Dope\n";
+    }
+    else if (score >= 250) {
+        std::cout << "You scored 250 or more. Nice\n";
+    }
+    else{
+        std::cout << "Cmon, you didnt even try. Only "<<score<<"? Ridiculous. :/\n";
     }
 
     return 0;
